b1d539ca27478eddeff1c080c0d759ad8e5eb40d
Paperfold <dreaming.paperfold@gmail.com>
2012-03-28 23:56:15 +0200

    add files

---
+++ drawceptional.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Drawceptional
+// @version     0.1
+// @namespace   Paperfold
+// @description Adds various extra things while drawing on Drawception.
+// @icon        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAadEVYdFNvZnR3YXJlAFBhaW50Lk5FVCB2My41LjEwMPRyoQAAALBJREFUWEftl20KgCAMhr2cHqVrdrTVAkN0q+n8+rFgUBJ7Hl8z0AGAW1lL4ThxE9gzASe8QgiAJXydnCw9KOgY4bkEN44tqd3WJJBAjvse601iuEAGf8CpxFABBv4pka6magl+4KxEFwEhnJRQC1TCCwmVQCOclaj6BpRwUkIs0AleSIgF4rrle7r1OfbrJsD9qTnBIQLe+zMthJiAJWAJWALdEph5VNvzXGAJzEzgAriChnskGYqfAAAAAElFTkSuQmCC
+// @match       http://drawception.com/play/
+// ==/UserScript==
+
